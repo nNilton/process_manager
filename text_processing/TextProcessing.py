60603c647bf338b94f570b7ec5bc2bd8b223be61
Nilton Vieira <nnilton.vieira@gmail.com>
2022-05-29 13:51:26 -0300

    WIP lematizacao

--- text_processing/TextProcessing.py
+++ text_processing/TextProcessing.py
@@ -1,5 +1,9 @@
 import threading
 import re
+import spacy
+import pt_core_news_sm
+
+nlp = pt_core_news_sm.load()
 
 class TextProcessing(threading.Thread):
 
@@ -19,6 +23,10 @@ class TextProcessing(threading.Thread):
                 dataFrame[columnItem] = self.filterWordsByLength(dataFrame, column = columnItem)
 
                 dfReturn = dataFrame.dropna(subset=targetColumns, axis=0) #remover dps
+
+                dfReturn = self.proccessTokensDf(dfReturn, column = 'PEDIDO')
+
+
                 thID = threading.get_ident()
                 dfReturn.to_csv(f'perguntasProcessadas{thID}.csv')
                 return dfReturn
@@ -59,3 +67,56 @@ class TextProcessing(threading.Thread):
                     word_tmp = word_tmp + " " + word
             coluna_tmp[i] = word_tmp
         return coluna_tmp
+
+    def lemmatize(self, dataFrame, columnName):
+        print(f'Aplicando Lemmatização')
+        lemmaWords = []
+        for pedido in dataFrame[columnName]:
+            doc = nlp(''.join(str(item) for item in pedido))
+            temp = ''
+            for token in doc:
+                temp = temp + ' ' + token.lemma_
+                lemmaWords.append(temp.strip())
+        print(f'Lemmatização concluído(a)')
+        return lemmaWords
+
+    def stemmer(self, dataFrame, columnName):
+        print(f'Aplicando RSLPStemmer')
+        stemmer = RSLPStemmer()
+        tam_length = len(dataFrame)
+        coluna_tmp = [0] * tam_length
+        for i in range(tam_length):
+            doc = nlp(str(dataFrame.iloc[i][columnName]))
+            tokens = doc.text.split()
+            temp = ""
+            for token in tokens:
+                if token != "nan":
+                    temp = temp + " " + stemmer.stem(token)
+
+            coluna_tmp[i] = temp.strip()
+        print(f'RSLPStemmer concluído(a)')
+        return coluna_tmp
+
+    def removeStopWords(self, dataFrame, columnName):
+        print(f'Removendo stopwords')
+        tam_length = len(dataFrame)
+        coluna_tmp = [0] * tam_length
+        for i in range(tam_length):
+            coluna_tmp[i] = removeStopWordsAux(str(dataFrame.iloc[i][columnName]))
+        print(f'Stopwords removidas')
+        return coluna_tmp
+
+    def removeStopWordsAux(self, sentence):
+            stopwords = nltk.corpus.stopwords.words('portuguese')
+            phrase = []
+            for word in sentence.split(' '):
+                if word not in stopwords:
+                    phrase.append(word)
+            return ' '.join(str(item) for item in phrase)
+
+    def proccessTokensDf(self, dataFrame, column = 'PEDIDO'):
+        dataFrame[column] = self.lemmatize(dataFrame, column)
+        dataFrame[column] = self.stemmer(dataFrame, column)
+        dataFrame[column] = self.removeStopWords(dataFrame, column)
+        dataFrame[column] = dataFrame[column].map(lambda x: x.lower()) ####NO LUGAR DE DATA_FILTER DEVERIA SER DATAFRAME N???
+        return dataFrame
